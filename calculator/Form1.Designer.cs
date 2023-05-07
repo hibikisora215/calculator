b6a21b998ba7de668fb1324a5240ae0470ab5d57
hibikisora215 <hibikisora@gmail.com>
2023-05-07 22:40:37 +0900

    first commit

---
+++ calculator/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button btn1;
+            System.Windows.Forms.Button btn2;
+            System.Windows.Forms.Button btn3;
+            System.Windows.Forms.Button btn4;
+            System.Windows.Forms.Button btn5;
+            System.Windows.Forms.Button btn6;
+            System.Windows.Forms.Button btn7;
+            System.Windows.Forms.Button btn8;
+            System.Windows.Forms.Button btn9;
+            System.Windows.Forms.Button button5;
+            System.Windows.Forms.Button btn0;
+            System.Windows.Forms.Button button7;
+            System.Windows.Forms.Button btnEqual;
+            System.Windows.Forms.Button btnPlus;
+            System.Windows.Forms.Button btnMinus;
+            System.Windows.Forms.Button btnTimes;
+            System.Windows.Forms.Button btnDivide;
+            System.Windows.Forms.Button btnClear;
+            this.textEquation = new System.Windows.Forms.TextBox();
+            this.textAnswer = new System.Windows.Forms.TextBox();
+            btn1 = new System.Windows.Forms.Button();
+            btn2 = new System.Windows.Forms.Button();
+            btn3 = new System.Windows.Forms.Button();
+            btn4 = new System.Windows.Forms.Button();
+            btn5 = new System.Windows.Forms.Button();
+            btn6 = new System.Windows.Forms.Button();
+            btn7 = new System.Windows.Forms.Button();
+            btn8 = new System.Windows.Forms.Button();
+            btn9 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            btn0 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            btnEqual = new System.Windows.Forms.Button();
+            btnPlus = new System.Windows.Forms.Button();
+            btnMinus = new System.Windows.Forms.Button();
+            btnTimes = new System.Windows.Forms.Button();
+            btnDivide = new System.Windows.Forms.Button();
+            btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            btn1.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btn1.Location = new System.Drawing.Point(32, 312);
+            btn1.Name = "btn1";
+            btn1.Size = new System.Drawing.Size(50, 50);
+            btn1.TabIndex = 0;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            btn2.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btn2.Location = new System.Drawing.Point(88, 312);
+            btn2.Name = "btn2";
+            btn2.Size = new System.Drawing.Size(50, 50);
+            btn2.TabIndex = 1;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            btn3.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btn3.Location = new System.Drawing.Point(144, 312);
+            btn3.Name = "btn3";
+            btn3.Size = new System.Drawing.Size(50, 50);
+            btn3.TabIndex = 2;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            btn4.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btn4.Location = new System.Drawing.Point(32, 256);
+            btn4.Name = "btn4";
+            btn4.Size = new System.Drawing.Size(50, 50);
+            btn4.TabIndex = 3;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            btn5.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btn5.Location = new System.Drawing.Point(88, 256);
+            btn5.Name = "btn5";
+            btn5.Size = new System.Drawing.Size(50, 50);
+            btn5.TabIndex = 4;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            btn6.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btn6.Location = new System.Drawing.Point(144, 256);
+            btn6.Name = "btn6";
+            btn6.Size = new System.Drawing.Size(50, 50);
+            btn6.TabIndex = 5;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            btn7.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btn7.Location = new System.Drawing.Point(32, 200);
+            btn7.Name = "btn7";
+            btn7.Size = new System.Drawing.Size(50, 50);
+            btn7.TabIndex = 6;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            btn8.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btn8.Location = new System.Drawing.Point(88, 200);
+            btn8.Name = "btn8";
+            btn8.Size = new System.Drawing.Size(50, 50);
+            btn8.TabIndex = 7;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn9
+            // 
+            btn9.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btn9.Location = new System.Drawing.Point(144, 200);
+            btn9.Name = "btn9";
+            btn9.Size = new System.Drawing.Size(50, 50);
+            btn9.TabIndex = 8;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            button5.Location = new System.Drawing.Point(32, 368);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(50, 50);
+            button5.TabIndex = 9;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            btn0.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btn0.Location = new System.Drawing.Point(88, 368);
+            btn0.Name = "btn0";
+            btn0.Size = new System.Drawing.Size(50, 50);
+            btn0.TabIndex = 10;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            button7.Location = new System.Drawing.Point(144, 368);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(50, 50);
+            button7.TabIndex = 11;
+            button7.Text = "5";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // textEquation
+            // 
+            this.textEquation.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textEquation.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textEquation.ForeColor = System.Drawing.Color.White;
+            this.textEquation.Location = new System.Drawing.Point(32, 26);
+            this.textEquation.Multiline = true;
+            this.textEquation.Name = "textEquation";
+            this.textEquation.Size = new System.Drawing.Size(301, 76);
+            this.textEquation.TabIndex = 12;
+            this.textEquation.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textAnswer
+            // 
+            this.textAnswer.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textAnswer.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textAnswer.ForeColor = System.Drawing.Color.White;
+            this.textAnswer.Location = new System.Drawing.Point(32, 108);
+            this.textAnswer.Name = "textAnswer";
+            this.textAnswer.Size = new System.Drawing.Size(301, 26);
+            this.textAnswer.TabIndex = 13;
+            // 
+            // btnEqual
+            // 
+            btnEqual.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btnEqual.Location = new System.Drawing.Point(200, 368);
+            btnEqual.Name = "btnEqual";
+            btnEqual.Size = new System.Drawing.Size(50, 50);
+            btnEqual.TabIndex = 14;
+            btnEqual.Text = "=";
+            btnEqual.UseVisualStyleBackColor = true;
+            // 
+            // btnPlus
+            // 
+            btnPlus.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btnPlus.Location = new System.Drawing.Point(200, 312);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new System.Drawing.Size(50, 50);
+            btnPlus.TabIndex = 15;
+            btnPlus.Text = "+";
+            btnPlus.UseVisualStyleBackColor = true;
+            // 
+            // btnMinus
+            // 
+            btnMinus.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btnMinus.Location = new System.Drawing.Point(200, 256);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new System.Drawing.Size(50, 50);
+            btnMinus.TabIndex = 16;
+            btnMinus.Text = "-";
+            btnMinus.UseVisualStyleBackColor = true;
+            // 
+            // btnTimes
+            // 
+            btnTimes.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btnTimes.Location = new System.Drawing.Point(200, 200);
+            btnTimes.Name = "btnTimes";
+            btnTimes.Size = new System.Drawing.Size(50, 50);
+            btnTimes.TabIndex = 17;
+            btnTimes.Text = "×";
+            btnTimes.UseVisualStyleBackColor = true;
+            // 
+            // btnDivide
+            // 
+            btnDivide.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btnDivide.Location = new System.Drawing.Point(256, 200);
+            btnDivide.Name = "btnDivide";
+            btnDivide.Size = new System.Drawing.Size(50, 50);
+            btnDivide.TabIndex = 18;
+            btnDivide.Text = "÷";
+            btnDivide.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            btnClear.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            btnClear.Location = new System.Drawing.Point(88, 144);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new System.Drawing.Size(50, 50);
+            btnClear.TabIndex = 19;
+            btnClear.Text = "C";
+            btnClear.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(367, 450);
+            this.Controls.Add(btnClear);
+            this.Controls.Add(btnDivide);
+            this.Controls.Add(btnTimes);
+            this.Controls.Add(btnMinus);
+            this.Controls.Add(btnPlus);
+            this.Controls.Add(btnEqual);
+            this.Controls.Add(this.textAnswer);
+            this.Controls.Add(this.textEquation);
+            this.Controls.Add(button7);
+            this.Controls.Add(btn0);
+            this.Controls.Add(button5);
+            this.Controls.Add(btn9);
+            this.Controls.Add(btn8);
+            this.Controls.Add(btn7);
+            this.Controls.Add(btn6);
+            this.Controls.Add(btn5);
+            this.Controls.Add(btn4);
+            this.Controls.Add(btn3);
+            this.Controls.Add(btn2);
+            this.Controls.Add(btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textEquation;
+        private System.Windows.Forms.TextBox textAnswer;
+    }
+}
+
